76b1ae360ee030b3112fcfb1be2dac3fa0f82274
zer0cache@protonmail.com <ssfaleads@gmail.com>
2021-07-28 22:13:55 -0400

    .

--- pages/index.js
+++ pages/index.js
@@ -1,68 +1,24 @@
 import Head from 'next/head'
+// import Image from 'next/image'
+import axios from 'axios';
+import _ from 'lodash';
 
-export default function Home() {
+export default function Home(props) {
+  console.log(props);
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Fisherman</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
+          Assets
         </h1>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
         <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {props.assets.map(asset => <img src={asset.image_url || 'TODO'} alt="TODO" />)}
         </div>
       </main>
 
@@ -79,4 +35,19 @@ export default function Home() {
       </footer>
     </div>
   )
+}
+
+
+export async function getStaticProps(context) {
+  const owner = '0x97F6B9d7B870226A7A51e9F2D5637C51e4d00537';
+  const offset = 0;
+  const limit = 20;
+  const order = 'desc';
+  const url = `https://api.opensea.io/api/v1/assets?order_direction=${order}&offset=${offset}&=limit=${limit}&owner=${owner}`;
+
+  const { data } = await axios.get(url);
+  console.log('data: ', data);
+  return {
+    props: data
+  }
 }
