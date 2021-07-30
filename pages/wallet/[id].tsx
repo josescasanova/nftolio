94602e6def48e09812502c99ff5577407ea7397f
zer0cache@protonmail.com <ssfaleads@gmail.com>
2021-07-29 23:49:13 -0400

    .

--- pages/wallet/[id].tsx
+++ pages/wallet/[id].tsx
@@ -61,6 +61,43 @@ const getTotalInEth = (collection: Collection, assetCount: AssetCount) => {
   return count * floorPrice;
 };
 
+// TODO add "register function" --- CONTRACT https://etherscan.io/address/0xdfa76df49ce8b01562f3de48126b8b6377c4e0a6#code @ 0.01 eth
+// TODO hide "portfolio in eth" w tooltip or something to say register to reveal
+// TODO do mini note that says "just send .01 eth to this address from the wallet u wanna see"
+// TODO if registered then show the $
+
+// const ethereumButton = document.querySelector('.enableEthereumButton');
+// const sendEthButton = document.querySelector('.sendEthButton');
+
+// let accounts = [];
+
+// //Sending Ethereum to an address
+// sendEthButton.addEventListener('click', () => {
+//   ethereum
+//     .request({
+//       method: 'eth_sendTransaction',
+//       params: [
+//         {
+//           from: accounts[0],
+//           to: '0x2f318C334780961FB129D2a6c30D0763d9a5C970',
+//           value: '0x29a2241af62c0000',
+//           gasPrice: '0x09184e72a000',
+//           gas: '0x2710',
+//         },
+//       ],
+//     })
+//     .then((txHash) => console.log(txHash))
+//     .catch((error) => console.error);
+// });
+
+// ethereumButton.addEventListener('click', () => {
+//   getAccount();
+// });
+
+async function getAccount() {
+  accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+}
+
 export default function Home(props: Props) {
   console.log("props: ", props);
   if (!props.collections.length) {
