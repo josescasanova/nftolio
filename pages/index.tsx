5dd7d781db90ef19c3d0796116739fb82ac41579
zer0cache@protonmail.com <ssfaleads@gmail.com>
2021-07-29 20:27:28 -0400

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,144 +1,100 @@
-import Head from "next/head";
-// import Image from 'next/image'
-import axios from "axios";
-import _ from "lodash";
-import { GetServerSideProps } from "next";
+import React, { useState } from 'react';
+import { useRouter } from "next/router";
 
-interface Collection {
-  stats?: {
-    floor_price: number;
-  };
-  banner_image_url: string;
-  description: string;
-  discord_url: string;
-  external_url: string;
-  featured_image_url: string;
-  image_url: string;
-  name: string;
+export default function Home() {
+  const router = useRouter();
+  const [walletAddress, setWalletAddress] = useState('');
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (!walletAddress) return;
+    console.log('walletADdress: ', walletAddress);
+    router.push(`/wallet/${walletAddress}`);
+  }
 
-}
-
-interface Props {
-  collections: Collection[];
-}
-
-export default function Home(props: Props) {
-  console.log(props);
+  const onInputChange = (e) => {
+    setWalletAddress(e.target.value);
+  }
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Fisherman</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <div className="container mx-auto m-4">
-          <h1 className="text-6xl font-bold">Fisherman</h1>
+    <div className="bg-white py-16 sm:py-24">
+      <div className="relative sm:py-16">
+        <div aria-hidden="true" className="hidden sm:block">
+          <div className="absolute inset-y-0 left-0 w-1/2 bg-gray-50 rounded-r-3xl" />
+          <svg className="absolute top-8 left-1/2 -ml-3" width={404} height={392} fill="none" viewBox="0 0 404 392">
+            <defs>
+              <pattern
+                id="8228f071-bcee-4ec8-905a-2a059a2cc4fb"
+                x={0}
+                y={0}
+                width={20}
+                height={20}
+                patternUnits="userSpaceOnUse"
+              >
+                <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+              </pattern>
+            </defs>
+            <rect width={404} height={392} fill="url(#8228f071-bcee-4ec8-905a-2a059a2cc4fb)" />
+          </svg>
         </div>
-
-        {/* <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          {props.assets.map(asset => <img src={asset.image_url || 'TODO'} alt="TODO" />)}
-        </div> */}
-        <div className="flex flex-col">
-          <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-              <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                <table className="min-w-full divide-y divide-gray-200">
-                  <thead className="bg-gray-50">
-                    <tr>
-                      <th
-                        scope="col"
-                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                      >
-                        Collection
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                      >
-                        ETH Floor
-                      </th>
-                      <th scope="col" className="relative px-6 py-3">
-                        <span className="sr-only">Link</span>
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody className="bg-white divide-y divide-gray-200">
-                    {props.collections.map((asset: Collection) => (
-                      <tr key={asset.name}>
-                        <td className="px-6 py-4 whitespace-nowrap">
-                          <div className="flex items-center">
-                            <div className="flex-shrink-0 h-10 w-10">
-                              <img
-                                className="h-10 w-10 rounded-full"
-                                src={asset.image_url}
-                                alt={asset.name}
-                              />
-                            </div>
-                            <div className="ml-4">
-                              <div className="text-sm font-medium text-gray-900">
-                                {asset.name}
-                              </div>
-                            </div>
-                          </div>
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                          {asset.stats?.floor_price}
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                          {asset.external_url ? (
-                            <a
-                            href={asset.external_url}
-                            className="text-indigo-600 hover:text-indigo-900"
-                          >
-                            Link
-                          </a>
-                          ) : null}
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
+        <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
+          <div className="relative rounded-2xl px-6 py-10 bg-indigo-600 overflow-hidden shadow-xl sm:px-12 sm:py-20">
+            <div aria-hidden="true" className="absolute inset-0 -mt-72 sm:-mt-32 md:mt-0">
+              <svg
+                className="absolute inset-0 h-full w-full"
+                preserveAspectRatio="xMidYMid slice"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 1463 360"
+              >
+                <path
+                  className="text-indigo-500 text-opacity-40"
+                  fill="currentColor"
+                  d="M-82.673 72l1761.849 472.086-134.327 501.315-1761.85-472.086z"
+                />
+                <path
+                  className="text-indigo-700 text-opacity-40"
+                  fill="currentColor"
+                  d="M-217.088 544.086L1544.761 72l134.327 501.316-1761.849 472.086z"
+                />
+              </svg>
+            </div>
+            <div className="relative">
+              <div className="sm:text-center">
+                <h2 className="text-3xl font-extrabold text-white tracking-tight sm:text-4xl">
+                  Enter your wallet address
+                </h2>
+                <p className="mt-6 mx-auto max-w-2xl text-lg text-indigo-200">
+                  See how much your NFT portfolio is worth. Based on ETH Floor prices.
+                </p>
               </div>
+              <form onSubmit={onSubmit} className="mt-12 sm:mx-auto sm:max-w-lg sm:flex">
+                <div className="min-w-0 flex-1">
+                  <label htmlFor="wallet" className="sr-only">
+                    Wallet Address
+                  </label>
+                  <input
+                    onChange={onInputChange}
+                    value={walletAddress}
+                    id="wallet"
+                    type="input"
+                    className="block w-full border border-transparent rounded-md px-5 py-3 text-base text-gray-900 placeholder-gray-500 shadow-sm focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-indigo-600"
+                    placeholder="Enter your wallet address"
+                  />
+                </div>
+                <div className="mt-4 sm:mt-0 sm:ml-3">
+                  <button
+                    onClick={onSubmit}
+                    type="submit"
+                    className="block w-full rounded-md border border-transparent px-5 py-3 bg-indigo-500 text-base font-medium text-white shadow hover:bg-indigo-400 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-indigo-600 sm:px-10"
+                  >
+                    Submit
+                  </button>
+                </div>
+              </form>
             </div>
           </div>
         </div>
-      </main>
+      </div>
     </div>
-  );
+  )
 }
-
-const fetch = async (options) => {
-  const { owner, limit, offset } = options;
-  const url = `https://api.opensea.io/api/v1/collections?asset_owner=${owner}&offset=${offset}&limit=${limit}`;
-
-  const { data } = await axios.get(url);
-  console.log("data: ", data);
-  console.log(" ");
-  return data;
-};
-
-const fetchCollections = async () => {
-  const owner = "0x72e464537c954e5451e96b725fdf22105dcf4ff4";
-  const limit = 300;
-  // TODO fetch all assets/iterate till response is empty array
-  let offset = 0;
-
-  const data = await fetch({
-    owner,
-    limit,
-    offset,
-  });
-
-  return data;
-};
-
-export const getStaticProps: GetServerSideProps = async (context) => {
-  const collections = await fetchCollections();
-  return {
-    props: {
-      collections
-    },
-  };
-};
---
+++ pages/wallet/[id].tsx
@@ -0,0 +1,145 @@
+import Head from "next/head";
+// import Image from 'next/image'
+import axios from "axios";
+import _ from "lodash";
+import { GetServerSideProps } from "next";
+
+interface Collection {
+  stats?: {
+    floor_price: number;
+  };
+  banner_image_url: string;
+  description: string;
+  discord_url: string;
+  external_url: string;
+  featured_image_url: string;
+  image_url: string;
+  name: string;
+
+}
+
+interface Props {
+  collections: Collection[];
+}
+
+export default function Home(props: Props) {
+  console.log(props);
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>Fisherman</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        <div className="container mx-auto m-4">
+          {/* <h1 className="text-6xl font-bold">Fisherman</h1> */}
+        </div>
+
+        {/* <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
+          {props.assets.map(asset => <img src={asset.image_url || 'TODO'} alt="TODO" />)}
+        </div> */}
+        <div className="flex flex-col">
+          <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+              <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                <table className="min-w-full divide-y divide-gray-200">
+                  <thead className="bg-gray-50">
+                    <tr>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                      >
+                        Collection
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                      >
+                        ETH Floor
+                      </th>
+                      <th scope="col" className="relative px-6 py-3">
+                        <span className="sr-only">Link</span>
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className="bg-white divide-y divide-gray-200">
+                    {props.collections.map((asset: Collection) => (
+                      <tr key={asset.name}>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <div className="flex items-center">
+                            <div className="flex-shrink-0 h-10 w-10">
+                              <img
+                                className="h-10 w-10 rounded-full"
+                                src={asset.image_url}
+                                alt={asset.name}
+                              />
+                            </div>
+                            <div className="ml-4">
+                              <div className="text-sm font-medium text-gray-900">
+                                {asset.name}
+                              </div>
+                            </div>
+                          </div>
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                          {asset.stats?.floor_price}
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                          {asset.external_url ? (
+                            <a
+                            href={asset.external_url}
+                            className="text-indigo-600 hover:text-indigo-900"
+                          >
+                            Link
+                          </a>
+                          ) : null}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+const fetch = async (options) => {
+  const { owner, limit, offset } = options;
+  const url = `https://api.opensea.io/api/v1/collections?asset_owner=${owner}&offset=${offset}&limit=${limit}`;
+
+  const { data } = await axios.get(url);
+  console.log("data: ", data);
+  console.log(" ");
+  return data;
+};
+
+const fetchCollections = async () => {
+  const owner = "0x72e464537c954e5451e96b725fdf22105dcf4ff4";
+  const limit = 300;
+  // TODO fetch all assets/iterate till response is empty array
+  let offset = 0;
+
+  const data = await fetch({
+    owner,
+    limit,
+    offset,
+  });
+
+  return data;
+};
+
+export const getStaticProps: GetServerSideProps = async (context) => {
+  console.log('context: ', context);
+  const collections = await fetchCollections();
+  return {
+    props: {
+      collections
+    },
+  };
+};
