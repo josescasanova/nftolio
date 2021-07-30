bc289f549602e62ae28e49e53219d653035446a8
zer0cache@protonmail.com <ssfaleads@gmail.com>
2021-07-30 02:12:54 -0400

    .

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,7 @@
 import "tailwindcss/tailwind.css";
-import { Head } from "next/document";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_API_KEY}', {
-              page_path: window.location.pathname,
-            });
-          `,
-          }}
-        />
-      </Head>
-      <Component {...pageProps} />;
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
