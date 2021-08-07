3e1f5c1cebbbad3f9b437a1e51886a2671a162a3
zer0cache@protonmail.com <ssfaleads@gmail.com>
2021-08-07 00:52:04 -0400

    .

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -38,10 +38,10 @@ export default class BaseDocument extends Document {
             content="nftolio.xyz - liquid NFT portfolio tracker"
           />
           <meta property="og:description" content={description} />
-          <meta
+          {/* <meta
             property="og:image"
             content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
-          />
+          /> */}
 
           <meta property="twitter:card" content="summary_large_image" />
           <meta property="twitter:url" content="https://metatags.io/" />
@@ -50,10 +50,10 @@ export default class BaseDocument extends Document {
             content="nftolio.xyz - liquid NFT portfolio tracker"
           />
           <meta property="twitter:description" content={description} />
-          <meta
+          {/* <meta
             property="twitter:image"
             content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
-          />
+          /> */}
 
           <meta name="author" content="nftolio.xyz" />
         </Head>
