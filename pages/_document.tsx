62ca5b58180b569f5227b7af2ffcecf146c34f48
zer0cache@protonmail.com <ssfaleads@gmail.com>
2021-08-07 00:25:43 -0400

    .

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -22,10 +22,34 @@ export default class BaseDocument extends Document {
           `,
             }}
           />
+          <meta
+            name="title"
+            content="nftolio.xyz - liquid NFT portfolio tracker"
+          />
+          <meta
+            name="description"
+            content="Find out how much, and liquid, your NFT portfolio is based on ETH floor prices."
+          />
+          <meta
+            name="keywords"
+            content="nft, portfolio tracker, nft portfolio tracker, nft portfolio"
+          />
+          <meta name="robots" content="index, follow" />
+          <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+          <meta name="language" content="English" />
+          <meta name="revisit-after" content="7 days" />
+          <meta name="author" content="nftolio.xyz" />
+
           <meta name="twitter:card" content="summary" />
           <meta property="og:title" content="nftolio.xyz - NFT value tracker" />
-          <meta property="og:description" content="nftolio.xyz - NFT liquid value tracker. Find out how much your NFT portfolio based on ETH floor prices." />
-          <meta property="og:image" content="https://nftolio.xyz/images/portfolioimg.jpg" />
+          <meta
+            property="og:description"
+            content="nftolio.xyz - NFT liquid value tracker. Find out how much your NFT portfolio based on ETH floor prices."
+          />
+          <meta
+            property="og:image"
+            content="https://nftolio.xyz/images/portfolioimg.jpg"
+          />
         </Head>
         <body>
           <Main />
